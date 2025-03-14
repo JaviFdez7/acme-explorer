d9dec8c6917ef8a51d3a5a00004a733ae21513a8
Daniel Arriaza Arriaza <113770002+darkgigi@users.noreply.github.com>
2025-03-14 18:35:30 +0100

    feat trip display and image carousel components for improved layout and styling

--- src/app/components/trip/trip-display/trip-display.component.ts
+++ src/app/components/trip/trip-display/trip-display.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Trip } from '../../../models/trip.model';
 import { ImageCarouselComponent } from "../../img-carousel/img-carousel.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-trip-display',
-  imports: [ImageCarouselComponent],
+  imports: [ImageCarouselComponent, CommonModule],
   templateUrl: './trip-display.component.html',
   styleUrl: './trip-display.component.css'
 })
@@ -15,9 +16,9 @@ export class TripDisplayComponent implements OnInit {
     this.trip = new Trip();
     this.trip.ticker = '57-AB32';
     this.trip.title = 'Trip to the moon';
-    this.trip.description = 'A trip to the moon to see the stars';
+    this.trip.description = 'A trip to the moon to see the stars. A once in a lifetime opportunity!';
     this.trip.price = 1000;
-    this.trip.requirements = ['18+', 'Passport'];
+    this.trip.requirements = ['18+', 'Passport', 'Vaccination', 'Insurance', 'Space suit'];
     this.trip.startDate = new Date();
     this.trip.endDate = new Date();
     this.trip.pictures = [`https://placehold.co/600x400?text=${this.trip.title.split(' ').join('+')}+1`, `https://placehold.co/600x400?text=${this.trip.title.split(' ').join('+')}+2`];
@@ -30,6 +31,16 @@ export class TripDisplayComponent implements OnInit {
 
   getPictures() {
     return this.trip.pictures;
+  }
+
+  getStartDate(lan: string) {
+    const formatter = new Intl.DateTimeFormat(lan, { day: 'numeric', month: 'long', year: 'numeric' });
+    return formatter.format(this.trip.startDate);
+  }
+
+  getEndDate(lan: string) {
+    const formatter = new Intl.DateTimeFormat(lan, { day: 'numeric', month: 'long', year: 'numeric' });
+    return formatter.format(this.trip.endDate);
   }
 
   ngOnInit(): void {
