2155c6c4790716dea87f6981f458949f9b8e1db9
Daniel Arriaza Arriaza <113770002+darkgigi@users.noreply.github.com>
2025-04-06 20:52:41 +0200

    feat: add stage in trip creation

--- src/app/components/manager/trip-create/trip-create.component.ts
+++ src/app/components/manager/trip-create/trip-create.component.ts
@@ -10,10 +10,12 @@ import { IftaLabelModule } from 'primeng/iftalabel';
 import { CardModule } from 'primeng/card';
 import { TextareaModule } from 'primeng/textarea';
 import { Trip } from '../../../models/trip.model';
+import { AccordionModule } from 'primeng/accordion';
+import { StepperModule } from 'primeng/stepper';
 
 @Component({
   selector: 'app-manager-trip-create',
-  imports: [CommonModule, ButtonModule, InputTextModule, IftaLabelModule, CardModule, ReactiveFormsModule, TextareaModule],
+  imports: [CommonModule, ButtonModule, InputTextModule, IftaLabelModule, CardModule, ReactiveFormsModule, TextareaModule, AccordionModule, StepperModule],
   templateUrl: './trip-create.component.html',
   styleUrl: './trip-create.component.css'
 })
@@ -22,6 +24,8 @@ export class ManagerTripCreateComponent {
   loading = false;
   error : string | null = null;
   success : string | null = null;
+  currentStep = 1;
+  stages!: FormArray;
 
   constructor(private fb : FormBuilder, private tripService: TripService, private authService: AuthService, private route: Router) {
     this.createForm();
@@ -34,9 +38,40 @@ export class ManagerTripCreateComponent {
       startDate: ['', [Validators.required]],
       endDate: ['', [Validators.required]],
       requirements: this.fb.array([], Validators.required),
-      price: ['', [Validators.required, Validators.min(0), Validators.max(1000000), Validators.pattern('^[0-9]*$')]],
       pictures: this.fb.array([]),
     });
+
+    this.stages = this.fb.array([]);
+  }
+
+  get stagesForms() {
+    return this.stages.controls as FormGroup[];
+  }
+
+  addStage() {
+    const stageGroup = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      price: [0, [Validators.required, Validators.min(0), Validators.pattern('^[0-9]*$')]],
+    });
+    this.stages.push(stageGroup);  
+  }
+
+  removeStage(index: number) {
+    this.stages.removeAt(index);
+  }
+
+  calculateTotalPrice() {
+    return this.stages.controls.reduce((sum, stage) => sum + (stage.value.price || 0), 0);
+  }
+
+  disableNextStep() {
+    if (this.currentStep === 1) {
+      return this.tripForm.invalid;
+    } else if (this.currentStep === 2) {
+      return this.stages.length === 0 || this.stages.invalid || this.loading;
+    }
+    return true;
   }
 
   async onSubmit() {
@@ -45,14 +80,8 @@ export class ManagerTripCreateComponent {
       this.error = null; 
       const managerId = this.authService.getCurrentId();
       const managerRole = this.authService.getCurrentRole();
-      if (!managerId) {
-        console.error('Manager ID not found');
-        return;
-      }
-      if (managerRole !== 'MANAGER') {
-        console.error('User is not a manager');
-        return;
-      }
+      if (!managerId || managerRole !== 'MANAGER') return;
+
       const ticker = await this.tripService.generateUniqueTicker();
       const pictures = this.tripForm.value.pictures
       if (pictures.length === 0) {
@@ -63,19 +92,25 @@ export class ManagerTripCreateComponent {
         managerId,
         this.tripForm.value.title,
         this.tripForm.value.description,
-        this.tripForm.value.price,
+        this.calculateTotalPrice(),
         new Date(this.tripForm.value.startDate),
         new Date(this.tripForm.value.endDate),
         this.tripForm.value.requirements,
         this.tripForm.value.pictures,
-        undefined
+        undefined,
+        0,
+        false,
+        this.stages.value,
       )
+
+      console.log('Trip:', trip.object);
 
       this.tripService.addTrip(trip).then(() => {
         this.loading = false;
         this.success = 'Trip added successfully!';
-        this.error = null;
-        this.tripForm.reset(); 
+        this.tripForm.reset();
+        this.stages.clear();
+        this.currentStep = 0;
         this.route.navigate(['/manager/'+ managerId + '/trips']); // Navigate to the trip list page after successful addition
       }).catch((error) => {
         this.loading = false;
@@ -84,6 +119,7 @@ export class ManagerTripCreateComponent {
       });
     } else {
       this.tripForm.markAllAsTouched(); 
+      this.stages.markAllAsTouched?.();
       this.loading = false;
       console.log(this.tripForm.errors);
       console.log('Form is invalid');
