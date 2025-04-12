f1688e835443a9fd5b2fb4109aa186f525fbbf84
Rafael González <rafaelgonzalezcastillero@gmail.com>
2025-04-12 17:11:53 +0200

    Implemented paypal payment

--- src/app/components/application/application-details/application-details.component.ts
+++ src/app/components/application/application-details/application-details.component.ts
@@ -14,10 +14,11 @@ import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } 
 import { IftaLabelModule } from 'primeng/iftalabel';
 import { TextareaModule } from 'primeng/textarea';
 import { MessageModule } from 'primeng/message';
+import { IPayPalConfig, ICreateOrderRequest, NgxPayPalModule } from 'ngx-paypal';
 
 @Component({
   selector: 'app-application-details',
-  imports: [CommonModule, CardModule, ButtonModule, DividerModule, DialogModule, ReactiveFormsModule, IftaLabelModule, TextareaModule, MessageModule],
+  imports: [CommonModule, CardModule, ButtonModule, DividerModule, DialogModule, ReactiveFormsModule, IftaLabelModule, TextareaModule, MessageModule, NgxPayPalModule],
   templateUrl: './application-details.component.html',
   styleUrl: './application-details.component.css'
 })
@@ -26,7 +27,7 @@ export class ApplicationDetailsComponent implements OnInit {
   protected trip: Trip | undefined;
   protected cancelForm!: FormGroup;
   protected isVisibleCancelDialog = false;
-  protected isVisiblePayDialog = false;
+  protected payPalConfig?: IPayPalConfig;
 
   constructor(private route: ActivatedRoute, private router: Router, private authService: AuthService, private tripService: TripService, private applicationService: ApplicationService, private fb: FormBuilder) {
     this.application = new Application("", "");
@@ -37,7 +38,7 @@ export class ApplicationDetailsComponent implements OnInit {
   }
 
   ngOnInit() {
-    console.log(this.cancelForm);
+    this.initConfig();
     this.applicationService.getApplication(this.route.snapshot.params['applicationId']).subscribe((application: Application | undefined) => {
       if (!application) {
         this.router.navigate(['/not-found']);
@@ -46,8 +47,81 @@ export class ApplicationDetailsComponent implements OnInit {
       this.application = application;
       this.tripService.getTrip(this.application.trip).subscribe((trip: Trip | undefined) => {
         this.trip = trip;
+        if (trip && this.payPalConfig) {
+          this.payPalConfig.createOrderOnClient = (data) => ({
+            intent: 'CAPTURE',
+            purchase_units: [{
+              amount: {
+                currency_code: 'EUR',
+                value: trip.price.toString(),
+                breakdown: {
+                  item_total: {
+                    currency_code: 'EUR',
+                    value: trip.price.toString()
+                  }
+                }
+              },
+              items: [{
+                name: this.trip?.title || '',
+                quantity: '1',
+                category: 'DIGITAL_GOODS',
+                unit_amount: {
+                  currency_code: 'EUR',
+                  value: trip.price.toString()
+                }
+              }]
+            }]
+          } as ICreateOrderRequest)
+        }
       });
     });
+  }
+
+  initConfig(): void {
+    const total = this.trip?.price || 0;
+    this.payPalConfig = {
+      currency: 'EUR',
+      clientId: 'ASD_DhyqRnbAHiqtC_V9C-qGxvlEREIQcOTtg7EhMKa984PI2bRJarwIpKRldpNEuZnagTpriBg3ZbeG',
+      advanced: {
+        commit: 'true'
+      },
+      style: {
+        layout: 'vertical',
+        label: 'paypal',
+
+      },
+      onApprove: (data, actions) => {
+        actions.order.capture().then((details: any) => {
+          console.log('Transaction approved by ' + details.payer.name.given_name);
+          console.log('Transaction details: ', details);
+          console.log('Transaction data: ', data);
+          console.log('Transaction actions: ', actions);
+        });
+      },
+      onClientAuthorization: async (data: any) => {
+        console.log('Transaction completed by ' + data.payer.name.given_name);
+        if (this.application && this.trip) {
+          const updatedApplication = new Application(this.application.trip, this.application.actor, this.application.messages, ApplicationStatus.ACCEPTED, this.application.date, "", this.application.version + 1)
+
+          await this.applicationService.editApplication(this.application.id, updatedApplication).then(() => {
+            this.router.navigate(['/explorer', this.authService.getCurrentId() || "", "applications"])
+          }).catch((error) => {
+            console.error('Error updating application:', error);
+          }
+          )
+        } else {
+          console.error('Application or trip is undefined');
+        }
+      },
+      onCancel: (data, actions) => {
+        console.log('Transaction cancelled');
+        console.log('Transaction data: ', data);
+        console.log('Transaction actions: ', actions);
+      },
+      onError: (err) => {
+        console.log('Error on transaction', err);
+      },
+    };
   }
 
   getApplicationStatus() {
@@ -105,19 +179,6 @@ export class ApplicationDetailsComponent implements OnInit {
 
   goTripDetails() {
     this.router.navigate(['/trip', this.application?.trip]);
-  }
-
-  async pay() {
-    if (this.application && this.trip) {
-      const updatedApplication = new Application(this.application.trip, this.application.actor, this.application.messages, ApplicationStatus.ACCEPTED, this.application.date, "", this.application.version + 1)
-
-      await this.applicationService.editApplication(this.application.id, updatedApplication).then(() => {
-        this.router.navigate(['/explorer', this.authService.getCurrentId() || "", "applications"])
-      }).catch((error) => {
-        console.error('Error updating application:', error);
-      }
-      )
-    }
   }
 
   showCancelDialog() {
