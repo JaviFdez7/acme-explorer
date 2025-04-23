2027a7fb4e1e8e65d830bb22247ec12cf9b816b1
JaviFdez7 <javidelatorre400@gmail.com>
2025-04-23 06:38:51 +0200

    feat: Implement accordion display for explorer applications and group applications by status

--- src/app/components/application/application-list/application-list.component.ts
+++ src/app/components/application/application-list/application-list.component.ts
@@ -1,37 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { Application } from '../../../models/application.model';
 import { ApplicationDisplayComponent } from '../application-display/application-display.component';
 import { CommonModule } from '@angular/common';
 import { DataView } from 'primeng/dataview';
 import { ApplicationService } from '../../../services/application.service';
 import { AuthService } from '../../../services/auth.service';
+import { AccordionModule } from 'primeng/accordion';
+import { BadgeModule } from 'primeng/badge';
 
 @Component({
   selector: 'app-application-list',
-  imports: [CommonModule, ApplicationDisplayComponent, DataView],
+  imports: [CommonModule, ApplicationDisplayComponent, DataView, AccordionModule, BadgeModule],
   templateUrl: './application-list.component.html',
   styleUrls: ['./application-list.component.css']
 })
 export class ApplicationListComponent implements OnInit {
-  protected applicationList: Application[] = [];
+  protected applicationList: any[] = [];
 
   constructor(private applicationService: ApplicationService, private authService: AuthService) { }
 
   ngOnInit() {
-    this.applicationService.getApplicationsByActor(this.authService.getCurrentId() || '').subscribe((applications: Application[]) => {
+    this.applicationService.getApplicationsByActorIdGroupedByStatusPerTrip(this.authService.getCurrentId() || '').subscribe((applications: any[]) => {
       this.applicationList = applications;
     });
-  }
-
-  getApplicationList() {
-    return this.applicationList;
-  }
-
-  getApplication(index: number) {
-    return this.applicationList[index];
-  }
-
-  getApplicationCount() {
-    return this.applicationList.length;
   }
 }
